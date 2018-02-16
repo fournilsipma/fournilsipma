6cf4a7cda28da3a930d2ddf57a935df174981763
Félix Sipma <felix.sipma@no-log.org>
2018-02-16 10:39:54 +0100

    add date picker

--- shop-client/src/Main.purs
+++ shop-client/src/Main.purs
@@ -6,9 +6,9 @@ import Control.Monad.Eff           (Eff)
 import Control.Monad.Eff.Class     (liftEff)
 import Control.Monad.Eff.Console   (CONSOLE)
 import Control.Monad.Eff.Exception (EXCEPTION, throw)
+import Control.Monad.Eff.Now       (NOW, nowDateTime)
 import Control.Monad.Except        (runExcept)
 import Data.Foldable               (traverse_)
-import Data.JSDate                 as JSDate
 import Data.Either                 (Either(..))
 import DOM.Node.ParentNode         (QuerySelector(..))
 import Halogen.Aff                 as HA
@@ -19,12 +19,13 @@ import Network.HTTP.Affjax         (AJAX, get)
 import Data.Foreign.Generic        (genericDecodeJSON)
 
 
-main :: Eff (HA.HalogenEffects ( console :: CONSOLE, ajax :: AJAX )) Unit
+main :: Eff (HA.HalogenEffects ( console :: CONSOLE, ajax :: AJAX, now :: NOW )) Unit
 main = HA.runHalogenAff do
     HA.awaitLoad
     res <- get S.fournilShopJson
     p <- liftEff $ decodeShop (res.response)
-    traverse_ (runUI (S.shopUI p) unit) =<< HA.selectElement (QuerySelector "#fournil-shop-app")
+    d <- liftEff nowDateTime
+    traverse_ (runUI (S.shopUI p d) unit) =<< HA.selectElement (QuerySelector "#fournil-shop-app")
 
 decodeShop :: forall e. String -> Eff ( exception :: EXCEPTION | e) Shop
 decodeShop json = case runExcept (genericDecodeJSON myOptions json) of
